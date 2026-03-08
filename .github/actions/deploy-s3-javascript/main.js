746fcd42eb882a444506bea5cb0c176e1114db0f
rhrynko <rhrynko@rhrynko-mbp.local>
2026-03-08 12:15:22 +0200

    fixed imports

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-import core from "@actions/core";
-import github from "@actions/github";
-import exec from "@actions/exec";
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as exec from "@actions/exec";
 
 function run() {
   core.notice("Hello from the deploy-s3-javascript action!");
